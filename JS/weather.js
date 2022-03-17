5638edfd04316d30c564edc68faf131c7c7c6b61
geppetto <ad105geppetto@gmail.com>
2022-03-17 23:59:26 +0900

    update weather & city & temp

--- JS/weather.js
+++ JS/weather.js
@@ -2,6 +2,15 @@ function onGeoOk(position) {
     const lat = position.coords.latitude;
     const lng = position.coords.longitude;
     console.log('You live in', lat, lng)
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIkey}`;
+    fetch(url)
+        .then(response => response.json())
+        .then((data) => {
+            const weather = document.querySelector('#weather span: first-child')
+            const city = document.querySelector('#weather span: last-child')
+            city.innerText = data.name;
+            weather.innerText = `${data.weather[0].main} / ${data.main.temp}`
+        })
 }
 
 function onGeoErr() {
@@ -10,4 +19,3 @@ function onGeoErr() {
 
 navigator.geolocation.getCurrentPosition(onGeoOk, onGeoErr);
 
-fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIkey}`)
