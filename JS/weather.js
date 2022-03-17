ed69fbb12d7fcf8a8f9663bc8ba6678757f9678f
geppetto <ad105geppetto@gmail.com>
2022-03-18 00:27:36 +0900

    reupdate cuz Internal Server Error

--- JS/weather.js
+++ JS/weather.js
@@ -2,15 +2,8 @@ function onGeoOk(position) {
     const lat = position.coords.latitude;
     const lng = position.coords.longitude;
     console.log('You live in', lat, lng)
-    // const url = ``;
-    fetch(url)
-        .then(response => response.json())
-        .then((data) => {
-            const weather = document.querySelector('#weather span:first-child')
-            const city = document.querySelector('#weather span:last-child')
-            city.innerText = data.name;
-            weather.innerText = `${data.weather[0].main} / ${data.main.temp}`
-        })
+    const url = ``;
+   
 }
 
 function onGeoErr() {
